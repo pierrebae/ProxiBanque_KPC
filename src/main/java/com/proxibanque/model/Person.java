3bad6ed700fb45501974f658cf67e511e3708376
yorhan <clement_lacorte@hotmail.fr>
2016-12-17 09:34:25 +0100

    commit kat

--- src/main/java/com/proxibanque/model/Person.java
+++ src/main/java/com/proxibanque/model/Person.java
@@ -2,4 +2,35 @@ package com.proxibanque.model;
 
 public class Person {
 
+	private long id;
+	private String firstName;
+	private String lastName;
+	
+	
+	//constructors
+	public Person() {
+		super();
+	}
+	
+	public Person(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+	
+	//getters, setters
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
 }
