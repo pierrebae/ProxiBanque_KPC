b2c337aecc9b6af30fcfdcee736c6e1fbf6c565d
yorhan <clement_lacorte@hotmail.fr>
2016-12-20 15:46:49 +0100

    debut validation

--- src/main/java/com/proxibanque/aspect/DaoHistory.java
+++ src/main/java/com/proxibanque/aspect/DaoHistory.java
@@ -1,33 +1,33 @@
-package com.proxibanque.aspect;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.hibernate.HibernateException;
-import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
-
-@Component
-@Transactional
-public class DaoHistory {
-	@PersistenceContext(unitName = "persistenceUnit")
-	protected EntityManager entityManager;
-	
-	
-	public void persist(HistoryCreateClient hist) throws HibernateException {
-		getEntityManager().persist(hist);
-	}
-
-
-	public EntityManager getEntityManager() {
-		return entityManager;
-	}
-
-
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
-	
-	
-	
-}
+//package com.proxibanque.aspect;
+//
+//import javax.persistence.EntityManager;
+//import javax.persistence.PersistenceContext;
+//
+//import org.hibernate.HibernateException;
+//import org.springframework.stereotype.Component;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@Component
+//@Transactional
+//public class DaoHistory {
+//	@PersistenceContext(unitName = "persistenceUnit")
+//	protected EntityManager entityManager;
+//	
+//	
+//	public void persist(HistoryCreateClient hist) throws HibernateException {
+//		getEntityManager().persist(hist);
+//	}
+//
+//
+//	public EntityManager getEntityManager() {
+//		return entityManager;
+//	}
+//
+//
+//	public void setEntityManager(EntityManager entityManager) {
+//		this.entityManager = entityManager;
+//	}
+//	
+//	
+//	
+//}
--- src/main/java/com/proxibanque/aspect/History.java
+++ src/main/java/com/proxibanque/aspect/History.java
@@ -1,45 +1,45 @@
-package com.proxibanque.aspect;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-@Component
-@Aspect
-public class History {
-	
-	@Autowired
-	DaoHistory daoHist;
-	HistoryCreateClient historyCreateClient=new HistoryCreateClient();
-	
-	@Pointcut("execution(* com.proxibanque.dao.IDao.persist(..))")
-	public void myPointCut() {
-		
-	}
-	@Before("myPointCut()") 
-	public void makeHistory(){
-		historyCreateClient.setOperation_name("createclient");
-		daoHist.persist(historyCreateClient);
-	}
-//	public void applause() {
-//	System.out.println("BRAVO...CLAPCLAP...BRAVO");
-//	}
-	
-//	private String simpleDate;
+//package com.proxibanque.aspect;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//import org.aspectj.lang.annotation.AfterReturning;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.aspectj.lang.annotation.Before;
+//import org.aspectj.lang.annotation.Pointcut;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Aspect
+//public class History {
 //	
-//	public String date(){
-//		Date creationDate = new Date();
-//		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-//		simpleDate = dateFormat.format(creationDate);
-//		return simpleDate;
+//	@Autowired
+//	DaoHistory daoHist;
+//	HistoryCreateClient historyCreateClient=new HistoryCreateClient();
+//	
+//	@Pointcut("execution(* com.proxibanque.dao.IDao.persist(..))")
+//	public void myPointCut() {
+//		
 //	}
-
-	
-	
-}
+//	@Before("myPointCut()") 
+//	public void makeHistory(){
+//		historyCreateClient.setOperation_name("createclient");
+//		daoHist.persist(historyCreateClient);
+//	}
+////	public void applause() {
+////	System.out.println("BRAVO...CLAPCLAP...BRAVO");
+////	}
+//	
+////	private String simpleDate;
+////	
+////	public String date(){
+////		Date creationDate = new Date();
+////		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+////		simpleDate = dateFormat.format(creationDate);
+////		return simpleDate;
+////	}
+//
+//	
+//	
+//}
