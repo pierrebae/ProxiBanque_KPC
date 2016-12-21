8466973c52b7bf12ff14660053e4e1d4df9949d2
pierre <pierre.baele@gmail.com>
2016-12-21 14:15:12 +0100

    CorrectCommit

--- src/main/java/com/proxibanque/dao/DaoClient.java
+++ src/main/java/com/proxibanque/dao/DaoClient.java
@@ -3,12 +3,10 @@ package com.proxibanque.dao;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.proxibanque.model.BankAccount;
 import com.proxibanque.model.Client;
 
 @Component
 @Transactional
-public class DaoClient extends DaoImpl<Client> implements IDaoClient{
+public class DaoClient extends DaoImpl<Client> implements IDaoClient {
 
-	
 }
--- src/main/java/com/proxibanque/dao/DaoTransfer.java
+++ src/main/java/com/proxibanque/dao/DaoTransfer.java
@@ -6,12 +6,17 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.proxibanque.model.BankAccount;
 import com.proxibanque.model.Client;
 import com.proxibanque.model.Transfer;
 import com.proxibanque.service.ServiceAccount;
 
+
+@Component
+@Transactional
 public class DaoTransfer extends DaoImpl<Transfer>  implements IDaoTransfer{
 	
 	
